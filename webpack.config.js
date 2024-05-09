73833ef24216d42f687b8d9bf251e847fe810625
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-09 13:38:55 +0300

    allowedHosts: "all"

--- webpack.config.js
+++ webpack.config.js
@@ -48,10 +48,9 @@ module.exports = {
         }
     },
 
-    
+
     devServer: {
-        compress: true,
-        disableHostCheck: true,   // That solved it
+        allowedHosts: "all"
     
      }  
 };
